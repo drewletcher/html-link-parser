d96a58a33020f83456388611e2bf1b5dca0de4c9
drewlab <drew@dictadata.org>
2026-05-23 10:14:47 -0500

    version 1.1.11

---
+++ test/HtmlLinkParser_reuse.test.js
@@ -0,0 +1,72 @@
+/**
+ * test/testParser.js
+ */
+
+import HtmlLinkParser from "../lib/HtmlLinkParser.js";
+import fs from "node:fs/promises";
+import path from "node:path";
+import compareFiles from "./_compareFiles.js";
+import colors from 'colors';
+
+async function test(options) {
+  try {
+    let outputName = "reuse";
+
+    console.log(outputName);
+    let outputFile = "./test/output/HtmlLinkParser/" + outputName + ".json";
+    console.log("output: " + outputFile);
+
+    await fs.mkdir(path.dirname(outputFile), { recursive: true });
+    let fh = await fs.open(outputFile, "w");
+    fh.write('[\n');
+
+    let parser = new HtmlLinkParser(options);
+
+    parser.on("head", (head) => {
+      if (head.redirect)
+        console.log("head: ".yellow + JSON.stringify(head).yellow);
+    });
+
+    parser.on("data", (data) => {
+      fh.write(JSON.stringify(data) + ",\n");
+    });
+
+    parser.on("end", () => {
+      console.log("end".yellow);
+    });
+
+    parser.on("error", (err) => {
+      console.error("error: ".red + err.toString().red);
+    });
+
+    // parse each url in the list
+    for (let url of options.urls) {
+      console.log("parsing: " + url);
+      fh.write('"parsing: ' + url + '",\n');
+      await parser.parse(url);
+    }
+
+    fh.write('"fini"\n]\n');
+    fh.close();
+    let expectedFile = outputFile.replace("/output/", "/expected/");
+    let exitCode = compareFiles(outputFile, expectedFile, 2);
+    return exitCode;
+  }
+  catch (err) {
+    console.error(err);
+    return 1;
+  }
+}
+
+(async () => {
+  if (await test({
+    urls: [
+      "https://www.sos.alabama.gov/alabama-votes",
+      "https://www.sos.alabama.gov/alabama-votes/voter/election-information"
+    ],
+    terms: [
+      /.*vote.*/i,
+      /.*elect.*/i
+    ]
+  })) return 1;
+})();
